cd7452b696284aaae7295a352cc04a617a1d6da5
wisteriasomnus <1262098118@qq.com>
2018-06-09 23:14:05 +0800

    add bootstrap

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function($) {
+    "use strict";
+    
+    $('#container').pinto({
+        itemWidth:230,
+        gapX:10,
+        gapY:10,
+        onItemLayout: function($item, column, position) {
+        }
+    });
+    
+    $("#pintoInit").click(function(){
+        $('#container').pinto();
+    });
+    
+    $("#pintoDestroy").click(function(){
+        $('#container').pinto("destroy");
+    });
+});
