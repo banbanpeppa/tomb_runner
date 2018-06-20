bfaac2ab371572e299c6bfe1a9de5eb3918a250f
wisteriasomnus <1262098118@qq.com>
2018-06-20 20:19:34 +0800

    wisteria final for mainet

--- game.js
+++ game.js
@@ -32682,6 +32682,7 @@ THREE.SphereBufferGeometry.prototype.clone = function() {
 }
 ;
 GEMIOLI.Menu = function() {
+    refreshLocalStorage();
     GEMIOLI.Layer.call(this);
     var a = this;
     a.bottom = new GEMIOLI.DisplayObjectContainer();
@@ -32694,7 +32695,6 @@ GEMIOLI.Menu = function() {
             return
         }
         a.hide();
-        console.log("a hide and menu fade, then you start the game");
         GEMIOLI.SoundLoader.load("button").play();
         GEMIOLI.Play.show()
     });
@@ -32727,11 +32727,13 @@ GEMIOLI.Menu = function() {
     a.shop.x = 600;
     a.shop.y = -190;
     a.shop.addEventListener("click", function(b) {
+        refreshLocalStorage();
         if (!a.showing || GEMIOLI.Button.inFocus) {
             return
         }
+
         GEMIOLI.SoundLoader.load("button").play();
-        GEMIOLI.Shop.show()
+        GEMIOLI.Shop.show();
     });
     a.bottom.addChild(a.shop);
     a.title1 = GEMIOLI.AtlasQuad.fromRect(-447, -158, 895, 317, "atlases/menu.atlas", "title1");
@@ -33044,7 +33046,7 @@ GEMIOLI.Score = function() {
             a.hide();
             GEMIOLI.Application.pushLayer(GEMIOLI.Play);
             GEMIOLI.Play.show();
-            GEMIOLI.Play.fadeMusic(0.25, 1)
+            GEMIOLI.Play.fadeMusic(0.25, 1);
         })
     });
     a.shop = new GEMIOLI.Button(-138,-138,277,277,[66, 83]);
@@ -33056,10 +33058,13 @@ GEMIOLI.Score = function() {
             return
         }
         GEMIOLI.SoundLoader.load("button").play();
-        //alert("you got score");
-        var h = GEMIOLI.Utils.getInt("highscore", 0);
-        var c = GEMIOLI.Utils.getInt("coins", 0);
-        var s = GEMIOLI.Utils.getInt("score", 0);
+        
+        if (typeof webExtensionWallet === "undefined") {
+          $("#noExtension").attr("style", "display:block;");
+          $("#submitbutton").attr("disabled", true);
+        } else {
+          $("#submitbutton").attr("disabled", false);
+        }
         $("#myModal").modal("show");
         //GEMIOLI.Shop.show(a.shopAttract);
         //a.shopAttract = 0
@@ -33129,17 +33134,20 @@ GEMIOLI.Score.prototype.show = function(a) {
     d.coinsText.text = GEMIOLI.Play.coins.toString();
     d.coinsText.scaleX = d.coinsText.scaleY = 6 / Math.max(6, d.coinsText.text.length);
 
+    var dis = d.distanceText.text;
+    var from = $.cookie('myaddress');
+    dis = dis.substring(0, dis.indexOf("m"));
     $("#scoreSpan").val("" + d.score);
-    $("#distanceSpan").val("" + d.distanceText.text);
+    $("#distanceSpan").val("" + dis);
     $("#coinSpan").val("" + d.coinsText.text);
-    console.log("d.score="+d.score);
-    console.log("d.distanceText.text="+d.distanceText.text);
-    console.log("d.distanceText.scaleX="+d.distanceText.scaleX);
-    console.log("d.distanceText.scaleY="+d.distanceText.scaleY);
-    console.log("d.coinsText.text="+d.coinsText.text);
-    console.log("d.coinsText.scaleX="+d.coinsText.scaleX);
-    console.log("d.coinsText.scaleY="+d.coinsText.scaleY);
-
+    $("#sdchash").val("" + SHA256(d.score + dis + d.coinsText.text + from));
+    // console.log("d.score="+d.score);
+    // console.log("d.distanceText.text="+d.distanceText.text);
+    // console.log("d.distanceText.scaleX="+d.distanceText.scaleX);
+    // console.log("d.distanceText.scaleY="+d.distanceText.scaleY);
+    // console.log("d.coinsText.text="+d.coinsText.text);
+    // console.log("d.coinsText.scaleX="+d.coinsText.scaleX);
+    // console.log("d.coinsText.scaleY="+d.coinsText.scaleY);
     var c = GEMIOLI.Utils.getInt("score", 0);
     if (a) {
         d.score = c + 1
@@ -33309,23 +33317,31 @@ GEMIOLI.Shop = function() {
                 b.position.set(0, 0, 0);
                 GEMIOLI.Play.scene3D.add(b)
             }
-        } else {
-            GEMIOLI.Utils.setInt("coins", GEMIOLI.Utils.getInt("coins", 0) - a.playerTypes[a.currentType].cost);
-            GEMIOLI.Utils.setBool("skin" + a.currentType.toString(), true);
-            GEMIOLI.Play.playerType = a.currentType;
-            GEMIOLI.Utils.setInt("skin", GEMIOLI.Play.playerType);
-            GEMIOLI.SoundLoader.load("buy").play();
+            a.updateData();
             if (!GEMIOLI.Score.showing) {
-                GEMIOLI.Play.scene3D.remove(GEMIOLI.Play.player);
-                var b = GEMIOLI.Play.objectFromPool(GEMIOLI.Play.playerTypes[GEMIOLI.Play.playerType].id);
-                b.position.set(0, 0, 0);
-                GEMIOLI.Play.scene3D.add(b)
+                a.hide()
+            }
+        } else {
+            buySkin(a.currentType, a);
+            if (false) {
+                GEMIOLI.Utils.setInt("coins", GEMIOLI.Utils.getInt("coins", 0) - GEMIOLI.Play.playerTypes[a.currentType].cost);
+                GEMIOLI.Utils.setBool("skin" + a.currentType, true);
+                GEMIOLI.Play.playerType = a.currentType;
+                GEMIOLI.Utils.setInt("skin", GEMIOLI.Play.playerType);
+                GEMIOLI.SoundLoader.load("buy").play();
+                if (!GEMIOLI.Score.showing) {
+                    GEMIOLI.Play.scene3D.remove(GEMIOLI.Play.player);
+                    var b = GEMIOLI.Play.objectFromPool(GEMIOLI.Play.playerTypes[GEMIOLI.Play.playerType].id);
+                    b.position.set(0, 0, 0);
+                    GEMIOLI.Play.scene3D.add(b);
+                }
             }
         }
-        a.updateData();
-        if (!GEMIOLI.Score.showing) {
-            a.hide()
-        }
+        // 这段代码用于更新角色代码
+        // a.updateData();
+        // if (!GEMIOLI.Score.showing) {
+        //     a.hide()
+        // }
     });
     a.nameText = GEMIOLI.Text.fromAtlas(75, "fonts/shop/name.fnt", "atlases/shop.atlas", GEMIOLI.Text.CENTER_ALIGN, GEMIOLI.Text.CENTER_ALIGN, GEMIOLI.Text.CENTER_ALIGN);
     a.nameText.x = 0;
@@ -33549,3 +33565,102 @@ GEMIOLI.Shop.prototype.render = function() {
     GEMIOLI.Renderer.setViewport(0, 0, GEMIOLI.Application.innerWidth, GEMIOLI.Application.innerHeight)
 }
 ;
+
+function SHA256(s) {
+    var chrsz = 8;
+    var hexcase = 0;
+    function safe_add(x, y) {
+        var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+        var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+        return (msw << 16) | (lsw & 0xFFFF);
+    }
+    function S(X, n) { return (X >>> n) | (X << (32 - n)); }
+    function R(X, n) { return (X >>> n); }
+    function Ch(x, y, z) { return ((x & y) ^ ((~x) & z)); }
+    function Maj(x, y, z) { return ((x & y) ^ (x & z) ^ (y & z)); }
+    function Sigma0256(x) { return (S(x, 2) ^ S(x, 13) ^ S(x, 22)); }
+    function Sigma1256(x) { return (S(x, 6) ^ S(x, 11) ^ S(x, 25)); }
+    function Gamma0256(x) { return (S(x, 7) ^ S(x, 18) ^ R(x, 3)); }
+    function Gamma1256(x) { return (S(x, 17) ^ S(x, 19) ^ R(x, 10)); }
+    function core_sha256(m, l) {
+        var K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2);
+        var HASH = new Array(0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19);
+        var W = new Array(64);
+        var a, b, c, d, e, f, g, h, i, j;
+        var T1, T2;
+        m[l >> 5] |= 0x80 << (24 - l % 32);
+        m[((l + 64 >> 9) << 4) + 15] = l;
+        for (var i = 0; i < m.length; i += 16) {
+            a = HASH[0];
+            b = HASH[1];
+            c = HASH[2];
+            d = HASH[3];
+            e = HASH[4];
+            f = HASH[5];
+            g = HASH[6];
+            h = HASH[7];
+            for (var j = 0; j < 64; j++) {
+                if (j < 16) W[j] = m[j + i];
+                else W[j] = safe_add(safe_add(safe_add(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16]);
+                T1 = safe_add(safe_add(safe_add(safe_add(h, Sigma1256(e)), Ch(e, f, g)), K[j]), W[j]);
+                T2 = safe_add(Sigma0256(a), Maj(a, b, c));
+                h = g;
+                g = f;
+                f = e;
+                e = safe_add(d, T1);
+                d = c;
+                c = b;
+                b = a;
+                a = safe_add(T1, T2);
+            }
+            HASH[0] = safe_add(a, HASH[0]);
+            HASH[1] = safe_add(b, HASH[1]);
+            HASH[2] = safe_add(c, HASH[2]);
+            HASH[3] = safe_add(d, HASH[3]);
+            HASH[4] = safe_add(e, HASH[4]);
+            HASH[5] = safe_add(f, HASH[5]);
+            HASH[6] = safe_add(g, HASH[6]);
+            HASH[7] = safe_add(h, HASH[7]);
+        }
+        return HASH;
+    }
+    function str2binb(str) {
+        var bin = Array();
+        var mask = (1 << chrsz) - 1;
+        for (var i = 0; i < str.length * chrsz; i += chrsz) {
+            bin[i >> 5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i % 32);
+        }
+        return bin;
+    }
+    function Utf8Encode(string) {
+        string = string.replace(/\r\n/g, "\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return utftext;
+    }
+    function binb2hex(binarray) {
+        var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+        var str = "";
+        for (var i = 0; i < binarray.length * 4; i++) {
+            str += hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8 + 4)) & 0xF) +
+                hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8)) & 0xF);
+        }
+        return str;
+    }
+    s = Utf8Encode(s);
+    return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
+};
--- js/index.js
+++ js/index.js
@@ -1,14 +1,56 @@
 "use strict";
-var dappAddress = "n1jxW6mRZeGkAuNj44sQjAHJ45zHSoHVj8J";
-var netbase = "https://testnet.nebulas.io";
-
+var dappAddress = "n1koy3jwVv1RtZ3DkHTbH6VHWkZFT4aiNW3";
+var netbase = "https://mainnet.nebulas.io";
 
 window.onload = function () {
+
   if (typeof webExtensionWallet === "undefined") {
     $("#noExtension").attr("style", "display:block;");
     $("#submitbutton").attr("disabled", true);
   } else {
     $("#submitbutton").attr("disabled", false);
+  }
+};
+
+function getAccount() {
+  var NebPay = require("nebpay");
+  var nebPay = new NebPay();
+  nebPay.simulateCall(dappAddress, "0", "getAccount", "", {
+    listener: getAccountCB
+  });
+};
+function getAccountCB(cb) {
+  var NebPay = require("nebpay");
+  var nebPay = new NebPay();
+  if (cb == 'error: please import wallet file') {
+    serialNumber = nebPay.call(dappAddress, 0, "xxxxxxxx", "[]", { //使用nebpay的call接口去调用合约,
+      listener: null        //设置listener, 处理交易返回信息
+    });
+    return;
+  }
+  var err = cb.execute_err;
+  if (err !== "") {
+    alert("请使用Mainnet进行游戏");
+    return;
+  }
+  var result = JSON.parse(cb.result);
+  var isReload = false;
+  var cook = $.cookie('myaddress') + "";
+  var account = result;
+  if (account != undefined && cook == undefined && account.length > 10 && cook.length > 10 && cook != account) {
+    isReload = true;
+  }
+  if (cook == '') {
+    isReload = true;
+  }
+
+  if (account == undefined || account.length < 10) {
+    alert('注意：请使用谷歌浏览器、并且安装<a class="p-2 text-dark" target="_blank" href="https://github.com/ChengOrangeJu/WebExtensionWallet">星云钱包插件</a>来玩');
+  } else {
+    $.cookie('myaddress', account, { expires: 7 });
+    if (isReload) {
+      location.reload();
+    }
   }
 };
 
@@ -74,21 +116,153 @@ function readFile() {
   };
 };
 
-function getComments(id) {
+$(function () {
+  $("#submitbutton").click(function () {
+    var NebPay = require("nebpay");
+    var nebPay = new NebPay();
+    var score = parseInt($("#scoreSpan").val());
+    var distance = parseInt($("#distanceSpan").val());
+    var coin = parseInt($("#coinSpan").val());
+    var sdchash = $("#sdchash").val();
+
+    var to = dappAddress;
+    var value = "0";
+    var callFunction = "saveRecord";
+    var callArgs = [];
+    callArgs.push(score);
+    callArgs.push(distance);
+    callArgs.push(coin);
+    callArgs.push(sdchash);
+    $("#submitbutton").attr("disabled", true);
+    $("#myModal").modal("hide");
+    $("#mask").removeAttr("hidden");
+    nebPay.call(to, value, callFunction, JSON.stringify(callArgs), {
+      listener: cbPush
+    });
+    function cbPush(resp) {
+      if (resp) {
+        if (resp.txhash) {
+          var txhash = resp.txhash;
+          let trigger = setInterval(() => {
+            var nebulas = require("nebulas"),
+              Account = nebulas.Account,
+              neb = new nebulas.Neb();
+            neb.setRequest(new nebulas.HttpRequest(netbase));
+
+            neb.api.getTransactionReceipt({ hash: txhash }).then((receipt) => {
+              console.log('status', receipt.status);
+
+              if (receipt.status != 2) { //not in pending
+                console.log(JSON.stringify(receipt));
+                if (receipt.status == 1) {
+                  clearInterval(trigger);
+                  $("#mask").attr("hidden", true);
+                  toast("保存成功", 5000);
+                  refreshLocalStorage();
+                } else {
+                  clearInterval(trigger);
+                  $("#mask").attr("hidden", true);
+                  toast(receipt.execute_result, 5000);
+                }
+              } else {
+                console.log("wisteria log:" + JSON.stringify(receipt));
+              }
+            });
+          }, 2000);
+        } else {
+          $("#mask").attr("hidden", true);
+          toast(resp);
+        }
+      }
+    };
+  });
+});
+
+function buySkin(skinNumber, a) {
+  var NebPay = require("nebpay");
+  var nebPay = new NebPay();
+
+  var to = dappAddress;
+  var value = "0";
+  var callFunction = "buySkin";
+  var callArgs = [];
+  callArgs.push(skinNumber);
+  $("#mask").removeAttr("hidden");
+  nebPay.call(to, value, callFunction, JSON.stringify(callArgs), {
+    listener: cbPush
+  });
+  function cbPush(resp) {
+    if (resp) {
+      if (resp.txhash) {
+        var txhash = resp.txhash;
+        let trigger = setInterval(() => {
+          var nebulas = require("nebulas"),
+            Account = nebulas.Account,
+            neb = new nebulas.Neb();
+          neb.setRequest(new nebulas.HttpRequest(netbase));
+          neb.api.getTransactionReceipt({ hash: txhash }).then((receipt) => {
+            console.log('status', receipt.status);
+
+            if (receipt.status != 2) { //not in pending
+              console.log(JSON.stringify(receipt));
+              if (receipt.status != 1) {
+                clearInterval(trigger);
+                $("#mask").attr("hidden", true);
+                toast(receipt.execute_result, 5000);
+              } else {
+                clearInterval(trigger);
+                $("#mask").attr("hidden", true);
+                toast("购买成功", 5000);
+                GEMIOLI.Utils.setInt("coins", GEMIOLI.Utils.getInt("coins", 0) - GEMIOLI.Play.playerTypes[a.currentType].cost);
+                GEMIOLI.Utils.setBool("skin" + a.currentType, true);
+                GEMIOLI.Play.playerType = a.currentType;
+                GEMIOLI.Utils.setInt("skin", GEMIOLI.Play.playerType);
+                GEMIOLI.SoundLoader.load("buy").play();
+                if (!GEMIOLI.Score.showing) {
+                    GEMIOLI.Play.scene3D.remove(GEMIOLI.Play.player);
+                    var b = GEMIOLI.Play.objectFromPool(GEMIOLI.Play.playerTypes[GEMIOLI.Play.playerType].id);
+                    b.position.set(0, 0, 0);
+                    GEMIOLI.Play.scene3D.add(b);
+                }
+
+                a.updateData();
+                if (!GEMIOLI.Score.showing) {
+                    a.hide();
+                }
+              }
+            } else {
+              console.log("wisteria log:" + JSON.stringify(receipt));
+            }
+          });
+        }, 2000);
+      } else {
+        $("#mask").attr("hidden", true);
+        toast(resp);
+      }
+    }
+  };
+};
+
+function refreshLocalStorage() {
+  console.log("refreshLocalStorage");
+  getAccount();
   var nebulas = require("nebulas"),
     Account = nebulas.Account,
     neb = new nebulas.Neb();
   neb.setRequest(new nebulas.HttpRequest(netbase));
-  var from = Account.NewAccount().getAddressString();
+  //var from = Account.NewAccount().getAddressString();
+  var from = $.cookie('myaddress');
+  if (!from) {
+    console.log("没有获得地址信息");
+  }
   var value = "0";
   var nonce = "0";
   var gas_price = "1000000";
   var gas_limit = "2000000";
   var to = dappAddress;
   var value = "0";
-  var callFunction = "getCommentsById";
+  var callFunction = "getRunner";
   var callArgs = [];
-  callArgs.push(id);
   var contract = {
     function: callFunction,
     args: JSON.stringify(callArgs)
@@ -96,70 +270,66 @@ function getComments(id) {
   neb.api
     .call(from, dappAddress, value, nonce, gas_price, gas_limit, contract)
     .then(function (resp) {
-      commentShow(resp);
+      if (resp.execute_err === "" && resp.result != "404") {
+        clearLocalStorage();
+        setLocalStorage(resp);
+      } else {
+        clearLocalStorage();
+      }
     })
     .catch(function (err) {
-      //cbSearch(err)
-      console.log("error:" + err.message);
+      clearLocalStorage();
     });
 };
 
-function getPublishingBlog() {
-  var value = "0";
-  var nonce = "0";
-  var gas_price = "1000000";
-  var gas_limit = "2000000";
-  var to = dappAddress;
-  var value = "0";
-  var callFunction = "getTopLine";
-  var callArgs = [];
-  var contract = {
-    function: callFunction,
-    args: JSON.stringify(callArgs)
-  };
-  neb.api
-    .call(from, to, value, nonce, gas_price, gas_limit, contract)
-    .then(function (resp) {
-      BlogShow(resp);
-    });
-  // .catch(function(err) {
-  //   //cbSearch(err)
-  //   console.log("error:" + err);
-  // });
+function clearLocalStorage() {
+  GEMIOLI.Utils.setInt("coins", 0);
+  GEMIOLI.Utils.setInt("score", 0);
+  GEMIOLI.Utils.setInt("highscore", 0);
+  GEMIOLI.Utils.setInt("skin", 0);
+  for (var i = 1; i < 6; i++) {
+    GEMIOLI.Utils.setBool("skin" + i, false);
+  }
+}
+
+function setLocalStorage(resp) {
+  var runner = JSON.parse(resp.result);
+  var score = runner.bestScore;
+  var coins = runner.coins;
+  var highscore = runner.coins;
+  var skins = runner.skins;
+
+  for (var i = 1; i < skins.length; i++) {
+    GEMIOLI.Utils.setBool("skin" + skins[i], true);
+  }
+  GEMIOLI.Utils.setInt("coins", coins);
+  GEMIOLI.Utils.setInt("score", score);
+  GEMIOLI.Utils.setInt("highscore", coins);
+  GEMIOLI.Utils.setBool("tutorial", false);
+  // GEMIOLI.Utils.setInt("coins", GEMIOLI.Utils.getInt("coins", 0);
+  // GEMIOLI.Utils.setInt("score", d.score);
+  // GEMIOLI.Utils.setBool("skin" + a.currentType.toString(), true);
+}
+
+var showLoading = function (sec) {
+  $("#mask").removeAttr("hidden");
+  setTimeout('$("#mask").attr("hidden", true);', sec * 1000);
 };
 
-$(function () {
-  $("#submitbutton").click(function () {
-    if (typeof webExtensionWallet === "undefined") {
-      $("#noExtension").attr("style", "display:block;");
-      $("#submitbutton").attr("disabled", true);
-    } else {
-      $("#submitbutton").attr("disabled", false);
-    }
-    var NebPay = require("nebpay");
-    var nebPay = new NebPay();
-    var score = $("#scoreSpan").val();
-    var distance = $("#distanceSpan").val();
-    var coin = $("#coinSpan").val();
-    console.log(score + "," + distance + "," + coin);
-    var to = dappAddress;
-    var value = "0";
-    var callFunction = "publish";
-    var callArgs = [];
-    callArgs.push(score);
-    callArgs.push(distance);
-    callArgs.push(coin);
-    console.log(callArgs);
-    $("#submitbutton").val("记录保存中...");
-    $("#submitbutton").attr("disabled", true);
-    nebPay.call(to, value, callFunction, JSON.stringify(callArgs), {
-      listener: cbPush
-    });
-    function cbPush(resp) {
-      console.log("response of push: " + resp);
-      $("#submitbutton").attr("disabled", false);
-      $("#submitbutton").val("保存记录");
-      $("#myModal").modal("hide");
-    };
-  });
-});
+var toast = function (msg, duration) {
+  duration = isNaN(duration) ? 3000 : duration;
+  var m = document.createElement("div");
+  m.innerHTML = msg;
+  m.style.cssText =
+    "width:500px; text-align:center; padding:0 10px; height:52px; color:white; line-height:52px; border-radius:5px; position:fixed; top:0; left:0; right:0; bottom:0; margin:auto; z-index:998; background:rgba(0, 0, 0, 0.7); font-size:16px;";
+  document.body.appendChild(m);
+  setTimeout(function () {
+    var d = 0.5;
+    m.style.webkitTransition =
+      "-webkit-transform " + d + "s ease-in, opacity " + d + "s ease-in";
+    m.style.opacity = "0";
+    setTimeout(function () {
+      document.body.removeChild(m);
+    }, d * 1000);
+  }, duration);
+};
